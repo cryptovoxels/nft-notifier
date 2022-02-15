a9a61050b675085a6ef1cf8055fddf1b8e1c4b0e
Benjythebee <Benjy.larcher@aol.com>
2022-02-16 12:46:50 +1300

    add support erc20

--- src/HookHandler.ts
+++ src/HookHandler.ts
@@ -3,14 +3,14 @@ import { getCollectibleMetadata, getNameMetadata, getParcelMetadata, isCVCollect
 import express from 'express'
 import { ClientManager } from './ClientManager'
 
-export default async function hookHandler  (req: express.Request, res: express.Response,clientManager:ClientManager)  {
+export default async function hookHandler(req: express.Request, res: express.Response, clientManager: ClientManager) {
   console.log(req.headers)
   // if(!isValidSignature(req)){
   //   res.status(400).send('Nothing to see here')
   //   return
   // }
 
-  if(!('app' in req.body)){
+  if (!('app' in req.body)) {
     res.status(200).send('Nothing to see here')
     return
   }
@@ -19,105 +19,124 @@ export default async function hookHandler  (req: express.Request, res: express.R
 
   res.status(200).end() // reply quickly
 
-  if(!body.activity){
+  if (!body.activity) {
     // we currently dont care about dropped tx or mined tx;
     return
   }
 
   // handle the webhook content
-  for (const activity of body.activity){
+  for (const activity of body.activity) {
     console.log(activity)
-    if(activity.category!=='token' && activity.category!=='erc1155'){
-     continue
+
+    if(activity.category=='internal'){
+      continue
     }
 
-    let category:'token'|'collectible'|'coin'|'parcel' = 'token'
+    let category: 'token' | 'collectible' | 'coin' | 'parcel' = 'token'
+    let symbol = activity.asset
     let token_id = activity.erc721TokenId
     let metadata = null
-    let chain =body.network=='MAINNET'?1:body.network=='MATIC_MAINNET'?137:8007
+    let chain = body.network == 'MAINNET' ? 1 : body.network == 'MATIC_MAINNET' ? 137 : 8007
     const from = get42AddressFrom64(activity.fromAddress)
     const to = get42AddressFrom64(activity.toAddress)
-    const address =activity.rawContract.address
+    const address = activity.rawContract.address
 
-    if(activity.asset=='CVPA' && !!isCVContract(address)){
+    if (isCVContract(address)) {
       category = 'parcel'
-      if(activity.log){
-        token_id = parseInt(activity.log?.data,16).toString()
+      if (activity.log) {
+        token_id = parseInt(activity.log?.data, 16).toString()
         metadata = await getParcelMetadata(token_id)
 
-        if(token_id){
+        if (token_id) {
           // query a refresh of the parcel so the owner is always refreshed
           fetch(`${API}/parcels/${token_id}/query`)
         }
       }
-    }else if(activity.category == 'erc1155'){
-
-      if(!activity.erc1155Metadata){
+    } else if (activity.category == 'erc1155') {
+      if (!activity.erc1155Metadata) {
         continue
       }
       let isCollection = await isCVCollection(address)
-      if(isCollection){
 
-        for(const mt of activity.erc1155Metadata){
-          let metadata = await getCollectibleMetadata(chain,address,parseInt(mt.tokenId,16))
-          if(!metadata){
+      // There can be multiple transfers for erc1155
+      for (const mt of activity.erc1155Metadata) {
+        if (isCollection) {
+          let metadata = await getCollectibleMetadata(chain, address, parseInt(mt.tokenId, 16))
+          if (!metadata) {
             continue
           }
-          const msg = {
-            from,
-            to,
-            chain,
-            hash:activity.hash,
-            value:parseInt(mt.value),
-            category:'collectible',
-            contract:address,
-            token_id:parseInt(mt.tokenId,16).toString(),
-            metadata
-          }
+        }
+        const msg = {
+          from,
+          to,
+          chain,
+          symbol,
+          hash: activity.hash,
+          value: parseInt(mt.value),
+          category: isCollection ? 'collectible' : 'token',
+          contract: address,
+          token_id: parseInt(mt.tokenId, 16).toString(),
+          metadata,
+        }
 
-            const clients = clientManager.clients.filter((c)=>c.wallet.toLowerCase()==to.toLowerCase())
-            for(const client of clients){
-              if(client){
-                client.sendNotify(msg)
-              }
-            }
+        const clients = clientManager.clients.filter((c) => c.wallet.toLowerCase() == to.toLowerCase())
+        for (const client of clients) {
+          if (client) {
+            client.sendNotify(msg)
+          }
         }
       }
-
-
-    }else if(isCVName(address)){
+      // we sent the notifications, don't continue
+      break
+    } else if (isCVName(address)) {
       category = 'token'
-      if(activity.log){
-        token_id = parseInt(activity.log?.data,16).toString()
+      if (activity.erc721TokenId) {
+        token_id = parseInt(activity.erc721TokenId, 16).toString()
+      }
+      if (token_id) {
         metadata = await getNameMetadata(token_id)
       }
+    } else if (activity.category == 'token') {
+      if (activity.erc721TokenId) {
+        token_id = parseInt(activity.erc721TokenId, 16).toString()
+      }
+      if (!token_id) {
+        continue
+      }
+    } else if(activity.category=='external'){
+      category='coin'
+      if(['ETH','WETH','MATIC','USDT'].includes(activity.asset)){
+        token_id = activity.value.toString()
+      }else{
+        continue
+      }
+
     }
-    
+
     console.log(from)
     console.log(to)
     const msg = {
       from,
       to,
       chain,
-      hash:activity.hash,
-      value:activity.value,
+      symbol,
+      hash: activity.hash,
+      value: activity.value,
       category,
-      contract:address,
+      contract: address,
       token_id,
-      metadata
+      metadata,
     }
     // filter because there can be multiple clients with the same wallet
-    const clients = clientManager.clients.filter((c)=> c.wallet.toLowerCase()==to.toLowerCase())
-    for(const client of clients){
-      if(client){
+    const clients = clientManager.clients.filter((c) => c.wallet.toLowerCase() == to.toLowerCase())
+    for (const client of clients) {
+      if (client) {
         client.sendNotify(msg)
       }
-
     }
-
   }
 }
 
-const get42AddressFrom64 = (address:string)=>{
-  return address.length>43?'0x' + address.substring('0x000000000000000000000000'.length,address.length):address
+const get42AddressFrom64 = (address: string) => {
+  return address.length > 43 ? '0x' + address.substring('0x000000000000000000000000'.length, address.length) : address
 }
